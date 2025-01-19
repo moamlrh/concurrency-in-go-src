f1b9a07b5da0774410ccf01fe7ace19e3c7f9f59
moaml <63509020+moamlrh@users.noreply.github.com>
2025-01-19 16:33:25 +0300

    range update Go.v1.22

--- gos-concurrency-building-blocks/goroutines/fig-goroutine-closure-loop-correct.go
+++ gos-concurrency-building-blocks/goroutines/fig-goroutine-closure-loop-correct.go
@@ -5,14 +5,17 @@ import (
 	"sync"
 )
 
+// This will works fine on Go.v1.22 (range notes part)
+// See docs for details : https://tip.golang.org/doc/go1.22
 func main() {
 	var wg sync.WaitGroup
+	// This code will not cause a race condition, since `salutation` is a new variable for each iteration of the range loop.
 	for _, salutation := range []string{"hello", "greetings", "good day"} {
 		wg.Add(1)
-		go func(salutation string) { // <1>
+		go func() {
 			defer wg.Done()
 			fmt.Println(salutation)
-		}(salutation) // <2>
+		}()
 	}
 	wg.Wait()
 }
